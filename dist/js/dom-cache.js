2f5661d2626c894507534b4b8ee92c5bb1c876ab
Anthony <anthony.siletti@gmail.com>
2020-05-01 15:56:57 -0400

    fixed script placement in index.html

--- dist/js/dom-cache.js
+++ dist/js/dom-cache.js
@@ -45,17 +45,3 @@ const body = document.body,
   channelVideosSection = document.getElementById('channel-content-videos'),
   channelVideosUl = document.getElementById('channel-content-videos-list');
 /* ------------------------------------------------------------------------- */
-// classes
-const youtube = new Youtube(),
-  svUI = new SearchAndVideoUi(),
-  chUI = new ChannelsUi(),
-  googleAuth = new GoogleAuth();
-
-// Other variables
-let searchParameter = '';
-
-// Loads the Google API Client library for use in all other files and init client
-googleAuth.loadClient();
-
-// Allows page reload by clicking brand logo
-brand.addEventListener('click', () => window.location.reload());
---
+++ dist/js/init.js
@@ -0,0 +1,14 @@
+// classes
+const youtube = new Youtube(),
+  svUI = new SearchAndVideoUi(),
+  chUI = new ChannelsUi(),
+  googleAuth = new GoogleAuth();
+
+// Other variables
+let searchParameter = '';
+
+// Loads the Google API Client library for use in all other files and init client
+googleAuth.loadClient();
+
+// Allows page reload by clicking brand logo
+brand.addEventListener('click', () => window.location.reload());
